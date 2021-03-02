ac471c2c3d1bbb50f31d3786fd84f0739a1bf6cf
gorgevorgyan <68996502+gorgevorgyan@users.noreply.github.com>
2021-03-02 13:00:50 +0400

    Update client.py

--- client.py
+++ client.py
@@ -5,7 +5,8 @@ import struct
 import time
 import pickle
 import zlib
-
+import imutils
+import numpy as np
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('3.136.108.3', 9999))
 connection = client_socket.makefile('wb')
@@ -17,11 +18,14 @@ cam.set(4, 240);
 
 img_counter = 0
 
-encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 18]
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 20]
 
 while True:
     ret, frame = cam.read()
+    frame = imutils.resize(frame, width=450)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = np.dstack([frame, frame, frame])
+
     result, frame = cv2.imencode('.jpg', frame, encode_param)
 #    data = zlib.compress(pickle.dumps(frame, 0))
     data = pickle.dumps(frame, 0)
@@ -33,3 +37,4 @@ while True:
     img_counter += 1
 
 cam.release()
+
