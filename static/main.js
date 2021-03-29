56239f8bad69000153d7cf7f74af9639052fdf79
gorgevorgyan <68996502+gorgevorgyan@users.noreply.github.com>
2021-03-29 19:33:19 +0400

    Add files via upload

--- static/main.js
+++ static/main.js
@@ -237,3 +237,4 @@ function setValue(theValue) {
 
 
 }
+
--- static/script.js
+++ static/script.js
@@ -5,4 +5,6 @@ socket.on('toweb', function(args){
 	document.getElementById("battery").innerHTML = args.battery;
 	document.getElementById("acceleration").innerHTML = "X:"+args.acceleration.x+", Y:"+args.acceleration.x+", Z:"+args.acceleration.x;
 	document.getElementById("gyroscope").innerHTML = "X:"+args.gyroscope.x+", Y:"+args.gyroscope.x+", Z:"+args.gyroscope.x;
+
+
 });
