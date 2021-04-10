6a4cc618ef596f91be4cdd23fa06d0fa5e9a621f
Sahak <sahak.sahakyan2017@gmail.com>
2021-04-10 13:47:01 +0400

    Updated hardware

--- drone.py
+++ drone.py
@@ -1,15 +1,49 @@
 import socketio
 import time
 import random
+from numpy import interp
+
+hard = 0 #set 0 if you are testing only software 
+
+if hard: 
+    import pigpio
+    pi = pigpio.pi() 
 
 sio = socketio.Client()
+
+motors = [
+[13, 19],
+[21, 16]]
+
+motor_speed = [[900, 900], [900, 900]]
+
+def update_motor_speeds(start=False):
+    if hard:
+        if start:
+            for i in range(2):
+                for j in range(2):
+                    pi.set_servo_pulsewidth(motors[i][j], motor_speed[i][j])
+        else:
+            for i in motors: 
+                for j in i: 
+                    pi.set_servo_pulsewidth(j, 900)
+
+update_motor_speeds(start=True)
+
 @sio.event
 def connect():
     print('connection established')
     sio.emit('hello')
+
 @sio.on('todrone')
 def dronid(*args):
-    print(args)
+    data = args[0]
+    print(data)
+    motor_speed[0][0] = interp((float(data["z"]) + ( (float(data["X_config"]) * float(data["x"]) - float(data["Y_config"]) * float(data["y"])) )) * float(data['m1']) / 100, [0, 100], [1000, 2000])
+    motor_speed[0][1] = interp((float(data["z"]) - ( (float(data["X_config"]) * float(data["x"]) + float(data["Y_config"]) * float(data["y"])) )) * float(data['m2']) / 100, [0, 100], [1000, 2000])
+    motor_speed[1][0] = interp((float(data["z"]) + (float(data["X_config"]) * float(data["x"]) + float(data["Y_config"]) * float(data["y"]))) * float(data['m3']) / 100, [0, 100], [1000, 2000])
+    motor_speed[1][1] = interp((float(data["z"]) + ( (float(data["Y_config"]) * float(data["y"]) - float(data["X_config"]) * float(data["x"])) )) * float(data['m4']) / 100, [0, 100], [1000, 2000])
+    print(motor_speed)
 
 @sio.on('config')
 def dronid(*config):
@@ -43,7 +77,9 @@ def states():
         		          'gyroscope':{'x':str(0),'y':str(0),'z':str(0)},
 
         		 })
-        time.sleep(0.5)
+        if hard:
+            update_motor_speeds()
+        time.sleep(0.3)
 
 @sio.event
 def disconnect():
